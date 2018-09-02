c7abc3fc4cde6adac4733af4f13e071e08ad8d72
yunus <yunus@192.168.1.2>
2018-09-02 23:32:21 +0300

    #Version 3 - Fixed minor kick bugs.

--- src/cr/anticow/listeners/PacketPlayInBlockPlace.java
+++ src/cr/anticow/listeners/PacketPlayInBlockPlace.java
@@ -1,8 +1,6 @@
 package cr.anticow.listeners;
 
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
-import org.bukkit.entity.Player;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.ListenerPriority;
@@ -10,9 +8,6 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 
 import cr.anticow.AntiCow;
-import cr.utils.Util;
-import net.minecraft.server.v1_8_R3.ItemStack;
-import net.minecraft.server.v1_8_R3.NBTTagList;
 
 public class PacketPlayInBlockPlace extends PacketAdapter {
 	
@@ -26,28 +21,10 @@ public class PacketPlayInBlockPlace extends PacketAdapter {
 
 	@Override
 	public void onPacketReceiving(PacketEvent e){
-		Player p = e.getPlayer();
-
 		org.bukkit.inventory.ItemStack item = e.getPacket().getItemModifier().read(0);		
-		if(item == null || !item.getType().equals(Material.WRITTEN_BOOK)) return;
+		if(item == null || (!item.getType().equals(Material.WRITTEN_BOOK) && !item.getType().equals(Material.BOOK_AND_QUILL))) return;
 
-		ItemStack book = CraftItemStack.asNMSCopy(item);
-		if(!book.hasTag() || !book.getTag().hasKey("pages")) return;
-		
-		NBTTagList pages = book.getTag().getList("pages", 8);
-		
-		if(pages.size() > 50){
-			e.setCancelled(true);
-			Util.kickPlayer(p, "Çok fazla veri gönderimi yapýyorsunuz!");
-		}else if(pages.size() >= 1){
-			for(int i = 0; i < pages.size(); i++){
-				if(pages.getString(i).length() > 255){
-					e.setCancelled(true);
-					Util.kickPlayer(p, "Çok fazla veri gönderimi yapýyorsunuz!");
-					break;
-				}
-			}
-		}
+		e.setCancelled(true);
 	}
 	
 }
